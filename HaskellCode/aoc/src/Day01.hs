bb797c35bb1982065689d315e0f467e37c6a3c89
gerben <gerry2211@gmail.com>
2021-12-01 17:07:46 +0100

    Added black magic to make subsequent days easier

--- HaskellCode/aoc/src/Day01.hs
+++ HaskellCode/aoc/src/Day01.hs
@@ -1,4 +1,6 @@
 module Day01 where
+  import Util
+
   countIncreases :: Ord a => [a] -> Int
   countIncreases numbers = foldl checkEntry 0 $ zip (init numbers) (tail numbers)
 
@@ -6,29 +8,14 @@ module Day01 where
   checkEntry carry (x, y)   | x < y = carry + 1
                             | otherwise = carry
 
-  computeAnswer1FromFile :: String -> Int
-  computeAnswer1FromFile contents = answer
-                                  where
-                                    contentLines = lines contents
-                                    numbers = map read contentLines :: [Int]
-                                    answer = countIncreases numbers
-
-  computeAnswer2FromFile :: String -> Int
-  computeAnswer2FromFile contents = answer
-                                  where
-                                    contentLines = lines contents
-                                    numbers = map read contentLines :: [Int]
-                                    answer = countIncreases $ conv3 numbers
   conv3 :: Num a => [a] -> [a]
   conv3 [] = []
   conv3 [x] = [x]
   conv3 [x,y] = [x,y]
   conv3 l = zipWith (+) (init $ init l) $ zipWith (+) (init $ tail l) (tail $ tail l)
 
-  findAnswerDay1q1 = do
-    contents <- readFile "../data/2021_01/data"
-    putStrLn $ show $ computeAnswer1FromFile contents
+  findAnswerDay1q1 = runOnFile countIncreases parseLinesToInts "../data/2021_01/data"
+                  where parseLinesToInts = parseLinesToType :: (String -> [Int])
 
-  findAnswerDay1q2 = do
-    contents <- readFile "../data/2021_01/data"
-    putStrLn $ show $ computeAnswer2FromFile contents
+  findAnswerDay1q2 = runOnFile (countIncreases.conv3) parseLinesToInts "../data/2021_01/data"
+                  where parseLinesToInts = parseLinesToType :: (String -> [Int])
--- HaskellCode/aoc/src/Util.hs
+++ HaskellCode/aoc/src/Util.hs
@@ -11,3 +11,17 @@ module Util where
                     | otherwise = binSearchH larger y (i + (length smaller) + 1)
                     where
                       (smaller, (x:larger)) = splitAt (div (length xs) 2) xs
+
+  -- Parses lines in the file to the desired type (type may have to be hinted with ::)
+  parseLinesToType :: Read a => String -> [a]
+  parseLinesToType text = map read $ lines text
+
+  -- Runs the computationFn on an input file and prints the results
+  -- Arguments:
+  --  computationFn: gets input type a and returns b
+  --  parser: Parses the String of the input file to input type a
+  --  file path
+  runOnFile :: (Read a, Show b) => (a -> b) -> (String -> a) -> String -> IO ()
+  runOnFile computationFn parser path = do
+      contents <- readFile path
+      putStrLn $ show $ computationFn $ parser contents
