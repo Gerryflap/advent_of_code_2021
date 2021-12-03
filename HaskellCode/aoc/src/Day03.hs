9bae4d038fc6cc348da997bbcad30d6ba540a7bd
gerben <gerry2211@gmail.com>
2021-12-03 13:59:14 +0100

    Day3 Q2

--- HaskellCode/aoc/src/Day03.hs
+++ HaskellCode/aoc/src/Day03.hs
@@ -1,5 +1,6 @@
 module Day03 where
 import Util
+import Data.List
 
 parseLineToBinVec :: String -> [Int]
 parseLineToBinVec ('0':xs) = 0:parseLineToBinVec xs
@@ -29,3 +30,32 @@ computeD3Q1 l = gamma * epsilon
               where (gamma, epsilon) = computeGammaEpsilon l
 
 answerD3Q1 = runOnFile computeD3Q1 parseInputDay3 "../data/2021_03/data"
+
+-- Finds a rating given the binary numbers, a function from count of ones and count of zeroes to 1 or 0, and a carry
+findRating :: [[Int]] -> (Int -> Int -> Int) -> [Int] -> Int
+findRating [xs] _ carry = toNum $ (reverse carry) ++ xs
+findRating nums fn carry = findRating newNums fn (bit:carry)
+                    where
+                      zeros = length $ filter (\r -> 0 == head r) $ nums
+                      ones = (length nums) - zeros
+                      bit = fn ones zeros
+                      filteredNums = filter (\num -> head num == bit) nums
+                      newNums = map tail filteredNums
+
+o2rating :: Int -> Int -> Int
+o2rating o z    | o < z = 0
+                | o >= z = 1
+
+co2rating :: Int -> Int -> Int
+co2rating o z   | o >= z = 0
+                | o < z = 1
+
+
+
+computeD3Q2 :: [[Int]] -> Int
+computeD3Q2 nums = o2r * co2r
+                  where
+                     o2r = findRating nums o2rating []
+                     co2r = findRating nums co2rating []
+
+answerD3Q2 = runOnFile computeD3Q2 parseInputDay3 "../data/2021_03/data"
