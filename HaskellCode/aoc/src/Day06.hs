55e04f527ced5321eb98abe41053b20a7969e98d
gerben <gerry2211@gmail.com>
2021-12-06 12:57:20 +0100

    Day 6, Question 2

--- HaskellCode/aoc/src/Day06.hs
+++ HaskellCode/aoc/src/Day06.hs
@@ -1,7 +1,9 @@
 module Day06 where
 import Util
 import Data.List.Split
-import Data.Map
+import qualified Data.Map as M
+import Data.Maybe
+import Debug.Trace
 
 decreaseAndAdd :: Int -> [Int] -> [Int]
 decreaseAndAdd 0 [] = []
@@ -16,12 +18,37 @@ simulateDays :: Int -> [Int] -> [Int]
 simulateDays 0 l = l
 simulateDays n l = simulateDays (n-1) $ decreaseAndAdd 0 l
 
-computeD6Q1 :: [Int] -> Int
+computeD6Q1 :: [Int] -> Int;
 computeD6Q1 l = length $ simulateDays 80 l
 
 answerD6Q1 = runOnFile computeD6Q1 parseDay6 "../data/2021_06/data"
 
-computeD6Q2 :: [Int] -> Int
-computeD6Q2 l = length $ simulateDays 256 l
+type LanternMap = M.Map Int Int
 
-answerD6Q2 = runOnFile computeD6Q2 parseDay6 "../data/2021_06/test"
+decreaseAndAddEfficient :: LanternMap -> LanternMap
+decreaseAndAddEfficient m = snd $ foldl updateMapFor (m, M.empty) [0..8]
+
+updateMapFor :: (LanternMap, LanternMap) -> Int -> (LanternMap, LanternMap)
+updateMapFor (old, new) 0 = (old, updatedMap)
+                          where
+                            old0 = getCountVal $ M.lookup 0 old
+                            tempMap = M.insert 6 old0 new
+                            updatedMap = M.insert 8 old0 tempMap
+updateMapFor (old, new) n = (old, updatedMap)
+                          where
+                            oldV = getCountVal $ M.lookup n old
+                            newV = getCountVal $ M.lookup (n-1) new
+                            updatedMap = M.insert (n-1) (oldV + newV) new
+
+simulateDaysEfficient :: Int -> LanternMap -> LanternMap
+simulateDaysEfficient 0 m = m
+simulateDaysEfficient n m = simulateDaysEfficient (n-1) $ decreaseAndAddEfficient m
+
+processD6Q2Input :: [Int] -> LanternMap -> LanternMap
+processD6Q2Input [] m = m
+processD6Q2Input (x:xs) m = processD6Q2Input xs $ M.insert x (newCountVal $ M.lookup x m) m
+
+computeD6Q2 :: [Int] -> Int
+computeD6Q2 l = sum $ map snd $ M.toList $ simulateDaysEfficient 256 $ processD6Q2Input l M.empty
+
+answerD6Q2 = runOnFile computeD6Q2 parseDay6 "../data/2021_06/data"
--- HaskellCode/aoc/src/Util.hs
+++ HaskellCode/aoc/src/Util.hs
@@ -1,27 +1,37 @@
 module Util where
-  -- Returns index or -1 if element cannot be found, assumes sorted input
-  binSearch :: (Ord a, Num a) => [a] -> a -> Int
-  binSearch xs y = binSearchH xs y 0
 
-  -- Helper fn, takes a start index as well in order to be able to return an index
-  binSearchH :: (Ord a, Num a) => [a] -> a -> Int -> Int
-  binSearchH [] _ _ = -1
-  binSearchH xs y i  | x == y = i
-                    | x > y = binSearchH smaller y i
-                    | otherwise = binSearchH larger y (i + (length smaller) + 1)
-                    where
-                      (smaller, (x:larger)) = splitAt (div (length xs) 2) xs
+-- Returns index or -1 if element cannot be found, assumes sorted input
+binSearch :: (Ord a, Num a) => [a] -> a -> Int
+binSearch xs y = binSearchH xs y 0
 
-  -- Parses lines in the file to the desired type (type may have to be hinted with ::)
-  parseLinesToType :: Read a => String -> [a]
-  parseLinesToType text = map read $ lines text
+-- Helper fn, takes a start index as well in order to be able to return an index
+binSearchH :: (Ord a, Num a) => [a] -> a -> Int -> Int
+binSearchH [] _ _ = -1
+binSearchH xs y i  | x == y = i
+                  | x > y = binSearchH smaller y i
+                  | otherwise = binSearchH larger y (i + (length smaller) + 1)
+                  where
+                    (smaller, (x:larger)) = splitAt (div (length xs) 2) xs
 
-  -- Runs the computationFn on an input file and prints the results
-  -- Arguments:
-  --  computationFn: gets input type a and returns b
-  --  parser: Parses the String of the input file to input type a
-  --  file path
-  runOnFile :: (Show b) => (a -> b) -> (String -> a) -> String -> IO ()
-  runOnFile computationFn parser path = do
-      contents <- readFile path
-      putStrLn $ show $ computationFn $ parser contents
+-- Parses lines in the file to the desired type (type may have to be hinted with ::)
+parseLinesToType :: Read a => String -> [a]
+parseLinesToType text = map read $ lines text
+
+-- Runs the computationFn on an input file and prints the results
+-- Arguments:
+--  computationFn: gets input type a and returns b
+--  parser: Parses the String of the input file to input type a
+--  file path
+runOnFile :: (Show b) => (a -> b) -> (String -> a) -> String -> IO ()
+runOnFile computationFn parser path = do
+    contents <- readFile path
+    putStrLn $ show $ computationFn $ parser contents
+
+-- Computes the new counter value for a counter in a map
+newCountVal :: Maybe Int -> Int
+newCountVal Nothing = 1               -- Key not in map, set counter to 1
+newCountVal (Just x) = x + 1          -- Key in map, add 1 to counter
+
+getCountVal :: Maybe Int -> Int
+getCountVal Nothing = 0
+getCountVal (Just x) = x
